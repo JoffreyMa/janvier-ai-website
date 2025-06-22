abdd839aa2caf5cdab77e56098da7f0f92c6c8f2
JoffreyMa <joffreyma@yahoo.fr>
2025-06-22 11:52:51 +0200

    Add Tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.janvier-ai.co'
+  site: 'https://www.janvier-ai.co',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
